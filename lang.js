be6f38409331aa4eb12cd8a349c2dc4cdc86abf4
qpwoeiruty35829346238456-wq <qpwoeiruty35829346238456@gmail.com>
2026-07-14 18:12:22 +0300

    1234

--- lang.js
+++ lang.js
@@ -3,11 +3,18 @@ document.querySelectorAll("[data-i18n]").forEach(el => {
     const key = el.getAttribute("data-i18n");
     el.textContent = translations[lang][key];   
 })
+document.querySelectorAll("#governorate option[value]").forEach(opt => {
+    const val = opt.value;
+    if(governorates[lang][val]){
+        opt.textContent = governorates[lang][val];
+    }
+})
     document.documentElement.lang = lang;
 document.documentElement.dir = lang === "ar" ? "rtl" : "ltr";
 localStorage.setItem("selectedLang" , lang);
 }
-document.getElementById("lang").addEventListener("change" , function(){
-    setLanguage(this.value);
-})
+
+// document.getElementById("lang").addEventListener("change" , function(){
+//     setLanguage(this.value);
+// })
 setLanguage(localStorage.getItem("selectedLang") || "en")
