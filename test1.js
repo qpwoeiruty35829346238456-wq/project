bf2b47565c55744661fc31b9a73a34901a606b9d
qpwoeiruty35829346238456-wq <qpwoeiruty35829346238456@gmail.com>
2026-08-21 03:06:39 +0300

    save

--- test1.js
+++ test1.js
@@ -1,7 +1,7 @@
 let Button = document.getElementById("button");
 let Img = document.getElementById("img");
 Button.addEventListener("click" , function(){
-    window.location.href = "worker.html"
+    window.location.href = "password.html"
 })
 Img.addEventListener("click" , function(){
     window.location.href = "work.html"
